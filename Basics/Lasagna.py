a17b38665fefd039a201161239410f335ecc617d
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-12 23:10:33 -0400

    Creating modules

--- Basics/Lasagna.py
+++ Basics/Lasagna.py
@@ -1,14 +1,18 @@
+#an exercice from Exercism.com
+
 EXPECTED_BAKE_TIME = 40
 
-def bake_time_remaining(current_time):
-    """"""
-    return EXPECTED_BAKE_TIME - current_time
+class Lasagna_Exercise:
     
-def preparation_time_in_minutes(number_of_layers):
-    """"""
-    return number_of_layers * 2
+    def bake_time_remaining(current_time):
+        """"""
+        return EXPECTED_BAKE_TIME - current_time
+        
+    def preparation_time_in_minutes(number_of_layers):
+        """"""
+        return number_of_layers * 2
 
 
-def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time ):
-    """"""
-    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
+    def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time ):
+        """"""
+        return elapsed_bake_time + self.preparation_time_in_minutes(number_of_layers)
--- MAIN/Main.py
+++ MAIN/Main.py
@@ -1,2 +1,6 @@
-import Basics.Basics
+from Basics.Lasagna import Lasagna_Exercise
 
+
+lasagna = Lasagna_Exercise()
+
+print(lasagna.bake_time_remaining(2))

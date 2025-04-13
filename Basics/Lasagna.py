cf9a4295cef14d1350761564384f5205ea6cec04
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-12 23:34:55 -0400

    Class refactor

--- Basics/Lasagna.py
+++ Basics/Lasagna.py
@@ -1,18 +1,20 @@
 #an exercice from Exercism.com
 
-EXPECTED_BAKE_TIME = 40
 
 class Lasagna_Exercise:
     
-    def bake_time_remaining(current_time):
+    def bake_time_remaining(self, current_time):
         """"""
+        EXPECTED_BAKE_TIME = 40
         return EXPECTED_BAKE_TIME - current_time
         
-    def preparation_time_in_minutes(number_of_layers):
+    def preparation_time_in_minutes(self, number_of_layers):
         """"""
+        EXPECTED_BAKE_TIME = 40
         return number_of_layers * 2
 
 
-    def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time ):
+    def elapsed_time_in_minutes(self, number_of_layers, elapsed_bake_time ):
         """"""
+        EXPECTED_BAKE_TIME = 40
         return elapsed_bake_time + self.preparation_time_in_minutes(number_of_layers)

635f97b89b381bb693fdbc476d6f8d0165a90cb6
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-12 22:54:25 -0400

    Lasagna file

---
+++ Basics/Lasagna.py
@@ -0,0 +1,14 @@
+EXPECTED_BAKE_TIME = 40
+
+def bake_time_remaining(current_time):
+    """"""
+    return EXPECTED_BAKE_TIME - current_time
+    
+def preparation_time_in_minutes(number_of_layers):
+    """"""
+    return number_of_layers * 2
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time ):
+    """"""
+    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
